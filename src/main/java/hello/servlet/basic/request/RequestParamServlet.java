0b066dd7fa037504519aa7d1c1325532a87c41a1
김진영C <jin0kim326@douzone.com>
2023-01-21 16:34:02 +0900

    2.1 Http 요청 데이터 - Get 쿼리 파라미터

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,43 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * 1.파라미터 전송 기능
+ * http://localhost:8080/request-param?username=jinyoung?age=20
+ */
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("[전체 파라미터 조회] - 👉🏻");
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName +" >> " +request.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - ✅");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회] 👉");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println("[단일 파라미터 조회] ✅");
+
+        System.out.println("[이름이 같은 복수 파라미터 조회] 👉");
+        String[] usernames = request.getParameterValues("username");
+        for (String s : usernames) {
+            System.out.println("username = " + s);
+        }
+        System.out.println("[이름이 같은 복수 파라미터 조회] ✅");
+
+        response.getWriter().write("OK");
+    }
+}
